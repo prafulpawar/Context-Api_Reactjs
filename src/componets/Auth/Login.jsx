f0aaa48e13ca03533ece5cac3e06c74fbfce8e83
prafulpawar <prafuldukhi@gmail.com>
2025-02-18 16:49:09 +0530

    __Login_Done

--- src/componets/Auth/Login.jsx
+++ src/componets/Auth/Login.jsx
@@ -5,13 +5,12 @@ function Login() {
     <div className='flex h-screen w-screen items-center justify-center '>
 
 
-         <div className='border-2 border-red-600  '>
-
+         <div className='border-2 border-red-600 border-emerald-600 p-20 '>
 
              <form action="" className='flex flex-col items-center justify-center'>
                    <input type="email" placeholder='Enter Your Email' />
                    <input type="password" placeholder='Enter Password' />
-                   <button>Log in</button>
+                   <button className='mt-5 '>Log in</button>
              </form>
 
 
