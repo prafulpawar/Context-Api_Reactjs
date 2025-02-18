ad29032ef799274f0e2ec9bb922ca57b657a4e3e
prafulpawar <prafuldukhi@gmail.com>
2025-02-18 21:23:12 +0530

    __Done__UI

--- src/componets/Dashboard/AdminDashboard.jsx
+++ src/componets/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Other/Header'
 import CreateTask from '../Other/CreateTask'
+import Alltask from '../Other/Alltask'
 
 function AdminDashboard() {
   return (
     <div className='h-screen w-full p-7 '>
       <Header />
        <CreateTask/>
+       <Alltask/>
     </div>
   )
 }
