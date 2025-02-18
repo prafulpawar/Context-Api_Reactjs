e5577070d38665d38ef3a7407e66ecacebbf871c
prafulpawar <prafuldukhi@gmail.com>
2025-02-18 22:09:35 +0530

    __Added_Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './componets/Auth/Login'
 import EmployeeDashboard from './componets/Dashboard/EmployeeDashboard'
 import AdminDashboard from './componets/Dashboard/AdminDashboard'
-
+import { getLocalStorage, setLocalStorage } from './utils/localstorage'
 function App() {
+   const [user,setUser] = useState(null);
+   const handleLogin = () =>{
 
-     useEffect(()=>{
-           setLocalStorage()
-     })
+   }
 
+
+   
   return (
     <div className='' >
-        
-           <Login/>
+           {!user ? <Login/> :" " }
+           {/* <Login/> */}
            {/* <EmployeeDashboard/>
           <AdminDashboard/> */}
     </div>
