457d4962bf96d0c2cfee80a3b0896946674ece2b
prafulpawar <prafuldukhi@gmail.com>
2025-02-19 23:40:47 +0530

    __Tasks_Done

--- src/componets/TaskList/TaskList.jsx
+++ src/componets/TaskList/TaskList.jsx
@@ -5,20 +5,35 @@ import FailedTask from './FailedTask'
 import NewTask from './NewTask'
 
 
-function TaskList() {
+function TaskList({data}) {
+    console.log(data.tasks)
     return (
         <div id='tasklist' className='h-[55%] pl-20 overflow-x-auto w-full mt-10 flex items-center justify-start gap-5 flex-nowrap'>
         
         
           
-            <AcceptTask/>
-            <CompleteTask/>
-            <FailedTask/>
-            <NewTask/>
+          {
+            data.tasks.map((elem)=>{
+                if(elem.active){
+                    return <AcceptTask/>
+                }  
+                 if(elem.completed){
+                    return <CompleteTask/>
+                }
+
+                 if(elem.new){
+                    return <NewTask/>
+                }
+                if(elem.failed){
+                    return <failedTasks/>
+                }
+            })
+          }
 
 
         </div>
     )
 }
+
 
 export default TaskList
