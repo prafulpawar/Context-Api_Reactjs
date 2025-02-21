494bda0be072d834f24bd728a7b2f7f16ae248e2
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 21:28:42 +0530

    __Done

--- src/componets/Other/Alltask.jsx
+++ src/componets/Other/Alltask.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { AuthContext } from '../../context/AuthProvider'
+
 
 function Alltask({data}) {
+   const authData =   useContext(AuthContext)
+    console.log(authData.employees)
   return (
     <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto '>
+
+
         <div className='bg-green-400 py-2 mb-2 px-4 flex justify-between rounded'>
              <h2>Rahul </h2>
              <h3>Make A UI Design</h3>
@@ -34,6 +41,8 @@ function Alltask({data}) {
              <h3>Make A UI Design</h3>
              <h5>Status</h5>
         </div>
+
+
     </div>
   )
 }
