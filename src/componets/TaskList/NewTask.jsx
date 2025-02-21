b264d5bca8eaa85e92faf640a5097e85d8ec9da4
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 21:17:09 +0530

    __Done

--- src/componets/TaskList/NewTask.jsx
+++ src/componets/TaskList/NewTask.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 
-function NewTask() {
+function NewTask({data}) {
   return (
     <div>
       <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl p-5  '>
 
         <div className='flex justify-between items-center'>
           <h3 className='bg-red-600 text-sm px-3 py-1 rounded '>
-            High
+          {data.category}
           </h3>
 
           <h4 className='text-sm'>
-            20 feb 2024
+          {data.date}
           </h4>
 
         </div>
-        <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Vedio</h2>
+        <h2 className='mt-5 text-2xl font-semibold'>{data.task}</h2>
 
         <p className='text-sm mt-2 '>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe impedit nostrum consequuntur nam corporis facilis.
-
+        {data.description}
         </p>
 
         <div className='flex justify-between mt-4 '>
