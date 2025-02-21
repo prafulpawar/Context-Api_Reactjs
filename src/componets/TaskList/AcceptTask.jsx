64c9c9801c3da80de1e786dbc614a66c02299935
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 20:54:28 +0530

    __Done__AcceptTask

--- src/componets/TaskList/AcceptTask.jsx
+++ src/componets/TaskList/AcceptTask.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function AcceptTask({data}) {
-  
+
   console.log(data);
 
 
@@ -11,18 +11,18 @@ function AcceptTask({data}) {
 
         <div className='flex justify-between items-center'>
           <h3 className='bg-red-600 text-sm px-3 py-1 rounded '>
-            High
+             {data.category}
           </h3>
 
           <h4 className='text-sm'>
-            20 feb 2024
+            {data.date}
           </h4>
 
         </div>
-        <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Vedio</h2>
+        <h2 className='mt-5 text-2xl font-semibold'>{data.task}</h2>
 
         <p className='text-sm mt-2 '>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe impedit nostrum consequuntur nam corporis facilis.
+          {data.description}
         </p>
 
            <div className='flex justify-between mt-4 '>
