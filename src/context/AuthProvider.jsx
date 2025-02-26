93835be0e7be723fcfe1eb647c312920ca479114
prafulpawar <prafuldukhi@gmail.com>
2025-02-26 22:25:49 +0530

    __Basics

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -6,22 +6,6 @@ export const AuthContext  = createContext()
 function AuthProvider({children}) {
   const [userData,setUserData] = useState(null)
 
-//   useEffect(() => {
-//     const localData = getLocalStorage();
-
-//     // Agar localStorage me data nahi hai, to pehle save kar do
-//     if (!localData.employees || !localData.admin) {
-//         console.warn("LocalStorage empty, initializing...");
-//         setLocalStorage(); // First time storage setup
-//     }
-
-//     // Get fresh data after setting it
-//     const updatedData = getLocalStorage();
-//     console.log("Updated Auth Data:", updatedData);
-
-//     setUserData(updatedData);
-// }, []);
-
   useEffect(()=>{
     setLocalStorage()
     const {employees} = getLocalStorage();
